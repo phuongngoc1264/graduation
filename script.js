3a3d906ecea7316fe82c15ee57bc650834367da9
phuongngoc1264 <phuongngoc1264@gmail.com>
2026-06-03 20:08:36 +0700

    script.js

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 function openCard() {
-  document.getElementById("cover").style.display = "none";
-  document.getElementById("card").style.display = "flex";
+  const envelope = document.querySelector(".envelope");
+  const cover = document.getElementById("cover");
+  const card = document.getElementById("card");
+
+  envelope.classList.add("open");
+
+  setTimeout(() => {
+    cover.classList.add("hide-cover");
+  }, 700);
+
+  setTimeout(() => {
+    cover.style.display = "none";
+    card.style.display = "flex";
+  }, 1400);
 }
